5c7811d29c47787c6789c17a5779ea52d1d3c9b5
m0003r <m0003r@gmail.com>
2022-05-26 12:54:16 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# Fast2ban
+
+This is simple fail2ban-like replacement written in Rust.
+
+## Usage:
+```bash
+./banner <config.toml location>
+```
+
+Emits list of suspicious IPs to stdout, one per line, some information to stderr:
+
+```
+Using config file config.toml
+Config: Config {
+    log_file: "nginx.log",
+    log_regex: "^(?P<ip>\\d+\\.\\d+\\.\\d+\\.\\d+) - [^ ]+ \\[(?P<DT>[^\\]]+)\\] \"(\\w+) (?P<addr>[^ ]*) HTTP/[\\d.]+\" (?P<code>\\d+) \\d+ \"[^\"]+\" \"[^\"]+\" \"(?P<UA>[^\"]+)",
+    requests: 30,
+    period: 30,
+    date_format: "%d/%B/%Y:%H:%M:%S %z",
+}
+elapsed 398 ms, 100000 lines parsed, 0 datetime errors, 251256.28140703516 lines/s, banned = 565/3498
+```
+
+## Configuration
+
+Example config.toml:
+```toml
+# log file to parse. Pass '-' to use stdin
+log_file = 'nginx.log'
+
+# regexp for parsing log file. Must contain groups <ip> and <DT>, other groups are ignored for now
+log_regex = '^(?P<ip>\d+\.\d+\.\d+\.\d+) - [^ ]+ \[(?P<DT>[^\]]+)\] "(\w+) (?P<addr>[^ ]*) HTTP/[\d.]+" (?P<code>\d+) \d+ "[^"]+" "[^"]+" "(?P<UA>[^"]+)'
+
+# maximum number of requests for specific IP for specific time period
+requests = 30
+
+# time period in seconds
+period = 30
+
+# date format (see https://docs.rs/chrono/0.4.19/chrono/format/strftime/index.html for syntax).
+# Note that timezone is required (because of using https://docs.rs/chrono/0.4.19/chrono/struct.DateTime.html#method.parse_from_str method)
+date_format = '%d/%B/%Y:%H:%M:%S %z'
+```
+
+## Using IPset to efficiently ban IPs
+
+IPset is a fast and efficient way to ban IPs (compared to banning them one by one via separate iptables rules).
+
+
+1. Create IPset file:
+```bash
+ipset create banner hash:ip
+```
+
+2. Create iptables rule for banning IPs:
+```bash
+iptables -I INPUT -p tcp -m multiport --dports 80,443 -m set --match-set banner src -j DROP
+```
+
+3. Run something like this periodically:
+```
+# get last queries
+tail -n 500000 /var/log/nginx/access.log | grep '/ HTTP/' > nginx.log
+# create suspicious IPs list
+./banner > ips.txt
+# create restore file for IPset
+cat ips.txt xargs -n1 echo add banner > ipset-restore.txt
+# add IPs to IPset 
+ipset restore -exist < ipset-restore.txt
+
+# or in single line:
+tail -n 500000 /var/log/nginx/access.log | grep '/ HTTP/' > nginx.log && ./banner | xargs -n1 echo add banner | ipset restore -exist
+
+# or if using log_file = '-' to read from stdin:
+tail -n 500000 /var/log/nginx/access.log | grep '/ HTTP/' | ./banner | xargs -n1 echo add banner | ipset restore -exist
+```
---
+++ src/main.rs
@@ -0,0 +1,121 @@
+use std::collections::{HashMap, HashSet};
+use std::env::args;
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+use std::net::IpAddr;
+use chrono::{DateTime, Duration, FixedOffset};
+use regex::Regex;
+use serde_derive::Deserialize;
+
+struct RingBanBuffer {
+    last_queries: Vec<Option<DateTime<FixedOffset>>>,
+    last_query_index: usize,
+}
+
+impl RingBanBuffer {
+    fn new(ring_size: usize) -> RingBanBuffer {
+        RingBanBuffer {
+            last_queries: vec![None; ring_size],
+            last_query_index: 0,
+        }
+    }
+
+    fn add_query(&mut self, query: DateTime<FixedOffset>) -> Option<Duration> {
+        self.last_queries[self.last_query_index] = Some(query);
+        self.last_query_index = (self.last_query_index + 1) % self.last_queries.len();
+
+        if let Some(prev) = self.last_queries[self.last_query_index] {
+            Some(prev - query)
+        } else {
+            None
+        }
+    }
+}
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    log_file: String,
+    log_regex: String,
+    requests: usize,
+    period: usize,
+    date_format: String,
+}
+
+
+// read config from TOML file and return a Config struct
+fn read_config(config_file: &str) -> Config {
+    let config_str = std::fs::read_to_string(config_file).expect(format!("Failed to read config file {}", config_file).as_str());
+    toml::from_str(&config_str).expect(format!("Failed to parse config file {}", config_file).as_str())
+}
+
+fn main() -> io::Result<()> {
+    let config_file = args().nth(1).unwrap_or("config.toml".to_string());
+    eprintln!("Using config file {}", config_file);
+    // read config
+    let config = read_config(&config_file);
+    eprintln!("Config: {:#?}", config);
+
+    let reader: Box<dyn BufRead>;
+    if config.log_file == "-" {
+        reader = Box::new(BufReader::new(io::stdin()));
+    } else {
+        reader = Box::new(BufReader::new(File::open(config.log_file).expect("Failed to open log file")));
+    }
+
+    if !&config.log_regex.contains("(?P<ip>") {
+        panic!("log_regex must contain (?P<ip> ... ) group for IP address");
+    }
+    if !&config.log_regex.contains("(?P<DT>") {
+        panic!("log_regex must contain (?P<DT> ... ) group for datetime");
+    }
+    let re = Regex::new(&config.log_regex).expect("Failed to compile regex");
+
+    let start = std::time::Instant::now();
+    // let mut uas = HashSet::new();
+    let mut banned = HashSet::new();
+    let mut ban_tickets = HashMap::new();
+    let mut line_count = 0;
+    let mut dt_errors = 0;
+
+    for line in reader.lines() {
+        if let Err(_) = line {
+            eprintln!("Line read error, break!");
+            break;
+        }
+        let line = line?;
+        line_count += 1;
+        let captures = re.captures(&line);
+        if let Some(caps) = captures {
+            let dt = DateTime::parse_from_str(&caps["DT"], &config.date_format);
+            if let Err(_) = dt {
+                eprintln!("Failed to parse date {}", &caps["DT"]);
+                dt_errors += 1;
+                continue;
+            }
+            let dt = dt.unwrap();
+            //parse ip into ipv4
+            let ip = caps["ip"].parse();
+            if let Err(_) = ip {
+                eprintln!("Failed to parse ip {}", &caps["ip"]);
+                continue;
+            }
+            let ip: IpAddr = ip.unwrap();
+            if banned.contains(&ip) {
+                continue;
+            }
+            let duration = ban_tickets.entry(ip).or_insert(RingBanBuffer::new(config.requests)).add_query(dt);
+            if let Some(dur) = duration {
+                if dur < Duration::seconds(config.period as i64) {
+                    banned.insert(ip);
+                }
+            }
+        }
+    }
+    let elapsed = start.elapsed();
+    eprintln!("elapsed {} ms, {} lines parsed, {} datetime errors, {} lines/s, banned = {}/{}", elapsed.as_millis(), line_count, dt_errors, line_count as f64 / (elapsed.as_millis() as f64 / 1000.0), banned.len(), ban_tickets.len());
+
+    for i in banned {
+        println!("{}", i);
+    }
+    Ok(())
+}
